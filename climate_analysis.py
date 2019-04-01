adb7c0c14bd360fa39c879bd6fff227dc9688aff
parismoscow <parisyorkmoscow@gmail.com>
2019-04-01 11:36:06 -0500

    modified api links

--- climate_analysis.py
+++ climate_analysis.py
@@ -34,17 +34,17 @@ app = Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
-# @app.route("/")
-# def welcome():
-#     """List all available api routes."""
-#     return (
-#            f"Available Routes:<br/>"
-#            f"/api/v1.0/precipitation<br/>"
-#            f"/api/v1.0/stations<br/>"
-#            f"/api/v1.0/tobs<br/>"
-#            f"/api/v1.0/start_date<br/>"
-#            f"/api/v1.0/start_date/end_date"
-#        )
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+           f"Available Routes:<br/>"
+           f"/api/v1.0/precipitation<br/>"
+           f"/api/v1.0/stations<br/>"
+           f"/api/v1.0/tobs<br/>"
+           f"/api/v1.0/start_date<br/>"
+           f"/api/v1.0/start_date/end_date"
+       )
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     session = Session(engine)
